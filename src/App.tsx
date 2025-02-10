1f13087bd2fc45cf28030342e5eb28d086b6ac10
suriya-dev-maatson <suriyamaatson@gmail.com>
2025-02-10 12:08:05 +0530

    i

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-red-600 text-center">Maatson Suriya</h1>
+      <h1 className="text-red-600 text-center">Maatson Maritime</h1>
     </>
   );
 }
