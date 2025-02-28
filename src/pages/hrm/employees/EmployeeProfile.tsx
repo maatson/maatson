3d9cf639717f5237223f5553576c8d3d9d536415
suriya-dev-maatson <suriyamaatson@gmail.com>
2025-02-28 10:12:07 +0530

    suriya

--- src/pages/hrm/employees/EmployeeProfile.tsx
+++ src/pages/hrm/employees/EmployeeProfile.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import PrimaryButton from "../../../components/buttons/PrimaryButton";
-import { EditIcon } from "../../../components/icons/Icons";
+import {
+  EditIcon,
+  EmailIcon,
+  MessageIcon,
+} from "../../../components/icons/Icons";
+import employeeProfile from "/images/sample/employeeProfile.png";
+import PinkChip from "../../../components/chips/PinkChip";
+import PrimaryChip from "../../../components/chips/PrimaryChip";
+import GreyButton from "../../../components/buttons/GreyButton";
+import SecondaryButton from "../../../components/buttons/SecondaryButton";
 
 const EmployeeProfile: React.FC = () => {
   return (
@@ -21,7 +30,51 @@ const EmployeeProfile: React.FC = () => {
         <div className="flex gap-6">
           {/* profile-left */}
           <div className="max-w-[380px] w-full bg-grey-aw-50 rounded-xs p-6 flex flex-col gap-3">
-            
+            <div className="w-[332px] h-[332px] rounded-sm">
+              <img src={employeeProfile} alt="" className="rounded-sm" />
+            </div>
+
+            <div className="mx-auto flex flex-col gap-2 ">
+              <div className="flex flex-col gap-1">
+                <p className="text-lg  font-semibold text-grey-ab text-center">
+                  @andrew_202
+                </p>
+                <p className="text-sm text-grey-ab-400">Andrew Symons</p>
+              </div>
+              <div className="flex justify-center  gap-1">
+                <PinkChip
+                  label={"Customer Service"}
+                  size={"s"}
+                  variant={"fill"}
+                />
+                <PrimaryChip
+                  label={"Department"}
+                  size={"s"}
+                  variant={"primary"}
+                />
+              </div>
+            </div>
+
+            <div className="flex gap-12 ">
+              <div className="w-full">
+                <GreyButton
+                  label={"Send Mail"}
+                  size={"m"}
+                  variant={"primary"}
+                  leftIcon={<EmailIcon size={16} color="#121212" />}
+                  style="w-full"
+                />
+              </div>
+              <div className="w-full">
+                <SecondaryButton
+                  label={"Message"}
+                  size={"m"}
+                  variant={"primary"}
+                  leftIcon={<MessageIcon size={16} color="#0E0E0E" />}
+                  style="w-full"
+                />
+              </div>
+            </div>
           </div>
 
           {/* profile-right */}
