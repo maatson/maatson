edbd7d39b303b9d11cde2319303da256583e1788
maatson <exim@maatson.com>
2025-03-12 10:41:41 +0530

    sales and crm  added

---
+++ src/pages/salesAndMarketing/booking/ConvertToBooking.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConvertToBooking: React.FC = () => {
+  return <div>ConvertToBooking</div>;
+};
+
+export default ConvertToBooking;
---
+++ src/pages/salesAndMarketing/booking/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Booking: React.FC = () => {
+  return <div>Booking</div>;
+};
+
+export default Booking;
