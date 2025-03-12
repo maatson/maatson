26369adc0519110c612bdf80ad82a74ffd88a636
suriya-dev-maatson <suriyamaatson@gmail.com>
2025-03-12 11:52:48 +0530

    suriya

--- src/pages/salesAndMarketing/booking/BookingList.tsx
+++ src/pages/salesAndMarketing/booking/BookingList.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PrimaryButton from "../../../components/buttons/PrimaryButton";
+import { AddIcon } from "../../../components/icons/Icons";
 
 const BookingList: React.FC = () => {
-  return <div>BookingList</div>;
+  return (
+    <>
+      <div className="bg-grey-aw-50 shadow-lg rounded-xs">
+        <div className="flex p-3 justify-between items-center">
+          <p className="text-lg text-grey-ab-900 font-semibold">Booking List</p>
+          <Link to={"/booking/add"}>
+            <PrimaryButton
+              label={"Add Booking"}
+              size={"l"}
+              variant={"primary"}
+              leftIcon={<AddIcon color="#FCFCFC" />}
+            />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BookingList;
--- src/pages/salesAndMarketing/enquiry/EnquiryList.tsx
+++ src/pages/salesAndMarketing/enquiry/EnquiryList.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import PrimaryButton from "../../../components/buttons/PrimaryButton";
+import { AddIcon } from "../../../components/icons/Icons";
+import { Link } from "react-router-dom";
 
 const EnquiryList: React.FC = () => {
   return (
     <>
-      <div>EnquiryList</div>
+      <div className="bg-grey-aw-50 shadow-lg rounded-xs">
+        <div className="flex p-3 justify-between items-center">
+          <p className="text-lg text-grey-ab-900 font-semibold">Enquiry List</p>
+          <Link to={"/enquiry/add"}>
+            <PrimaryButton
+              label={"Add Enquiry"}
+              size={"l"}
+              variant={"primary"}
+              leftIcon={<AddIcon color="#FCFCFC" />}
+            />
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
