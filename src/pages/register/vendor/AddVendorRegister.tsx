8b5e92aad41be7b39e1748ef636500be27e266b5
suriya-dev-maatson <suriyamaatson@gmail.com>
2025-03-17 17:55:50 +0530

    form optimization with sample data

--- src/pages/register/vendor/AddVendorRegister.tsx
+++ src/pages/register/vendor/AddVendorRegister.tsx
@@ -57,7 +57,7 @@ const AddVendorRegister: React.FC = () => {
     setIsProductActive(true);
     setIsServiceActive(false);
   };
-  const handleService = () => {
+  const handleService = () => {  
     setIsProductActive(false);
     setIsServiceActive(true);
   };
