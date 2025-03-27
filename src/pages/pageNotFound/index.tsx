5e55cadb8780bdae7f963cb3d54715bf007596e5
maatson <exim@maatson.com>
2025-03-27 13:18:07 +0530

    pricing routes

--- src/pages/pageNotFound/index.tsx
+++ src/pages/pageNotFound/index.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import FollowUps from "../../components/followups/FollowUps";
-import Header from "../../components/header/Header";
-import SideBar from "../../components/sideBar/SideBar";
-import Footer from "../../components/footer/Footer";
-import AddSchedule from "../customerService/sea-air-schedule/sea-freight/updateSchedule/AddSeaScheduleForm";
 
 const PageNotFound: React.FC = () => {
   return (
     <>
-       <p>PageNotFound 404</p>;
+      <p>PageNotFound 404</p>
       <div className="px-10 w-fit mx-auto">
-        <FollowUps
+        {/* <FollowUps
           onClose={function (): void {
             throw new Error("Function not implemented.");
           }}
-        />
+        /> */}
       </div>
     </>
   );
